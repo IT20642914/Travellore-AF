4a834e234a82ed770aedf2fbfa9cd2748d7181e8
avishka05 <88711826+avishka05@users.noreply.github.com>
2023-03-25 16:45:11 +0530

    created home page

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,14 +1,20 @@
 
 import './App.css';
 
+import { BrowserRouter , Routes,Route } from 'react-router-dom';
+import Home from './pages/Home';
 function App() {
-  return (
-    <div className="App">
-      
-          Learn React
+  return (<BrowserRouter>
+
+  <Routes>
+
+  <Route path="/" element={<Home/>} />
 
 
-    </div>
+
+  </Routes>
+  </BrowserRouter>
+ 
   );
 }
 
