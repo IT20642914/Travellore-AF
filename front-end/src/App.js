03818ffad18a05a5ec8d79d4891ad81ce5851aa1
avishka05 <88711826+avishka05@users.noreply.github.com>
2023-05-06 21:01:12 +0530

    fix:change code  and routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,32 +1,37 @@
 import "./App.css";
 import Dashbord from "./Components/Dashboard/dashbord"
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route ,Navigate   } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login/Login";
+import LocalBussiness from "./pages/LocalBussiness/LocalB";
 import SignupScreen from "./pages/Register/Register" 
 import { useSelector } from 'react-redux';
 import { ToastContainer, toast } from 'react-toastify';
 import PublicRoutes from "./routes/PublicRoutes"
+import { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router';
+import { Box } from "@mui/material";
+
 function App() {
   const  IsLogin= useSelector((state)=> state.login.isLoggedIn)
   const  isAdmin =useSelector((state)=> state.login.IsAdmin)
   console.log("Admi",isAdmin)
   console.log("IsLogin",IsLogin)
- 
+
+
   return (
-    <BrowserRouter>
+  <Box>
     <ToastContainer/>
 
-
-
-    {/* {IsLogin&& isAdmin &&<Dashbord/>} */}
- {/* {IsLogin&& !isAdmin && <Home/>} */}
-
       <Routes>
-
       <Route path="/" element={<PublicRoutes/>} > 
       <Route path="/" element={<Home/>} /> 
       </Route>
+  
+{  IsLogin&& isAdmin && <Route path="/adminhome" element={<Dashbord/>} > 
+      <Route path="/adminhome" element={<Home/>} /> 
+      <Route path="/adminhome/localb" element={<LocalBussiness/>} /> 
+      </Route>}
 
 
       { !IsLogin &&<Route path="/" element={<PublicRoutes/>} > 
@@ -34,9 +39,15 @@ function App() {
       <Route path="/register" element={<SignupScreen/>} /> 
       </Route>}
  
+
+
+
       </Routes>
 
-    </BrowserRouter>
+
+
+      </Box>
+
   );
 }
 
