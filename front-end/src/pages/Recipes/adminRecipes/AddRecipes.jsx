6fe4e07d508e8636389b6a0751577f11dfa569da
Chathi19 <79710080+Chathi19@users.noreply.github.com>
2023-05-07 23:19:27 +0530

    fix add recipes

--- front-end/src/pages/Recipes/adminRecipes/AddRecipes.jsx
+++ front-end/src/pages/Recipes/adminRecipes/AddRecipes.jsx
@@ -7,63 +7,66 @@ import { grid } from "@mui/system";
 
 const AddRecipes = () => {
   return (
-    <Grid className={Styles.DetailGrid}>
-      <Box className={Styles.HeaderText}>
-    <div className="AddRecipesPage flex">
-      <div className="container flex">
+    <div className="card">
+      <div className="AddRecipesPage flex">
+        <div className="container flex">
+          <h2 >Add Recipes</h2>
+          <div class="card-body">
+            <form action="" className="form grid">
+              <div class="form-group">
+                <label for="name">Name</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="inputName"
+                  placeholder="Enter Name"
+                />
+              </div>
 
-        <h2>Add Recipes</h2>
-        <form action="" className="form grid">
-          <div class="form-group">
-            <label for="name">Name</label>
-            <input
-              type="text"
-              class="form-control"
-              id="inputName"
-              placeholder="Enter Name"
-            />
+              <div class="form-group">
+                <label for="category">Category</label>
+                <select id="category" class="form-control">
+                  <option selected>Choose...</option>
+                  <option>Sinhalese</option>
+                  <option>Tamil</option>
+                  <option>Muslim</option>
+                </select>
+              </div>
+
+              <div class="form-group">
+                <label for="ingredients">Ingredients</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="ingredients"
+                  placeholder="Enter ingredients"
+                />
+              </div>
+
+              <div class="form-group">
+                <label for="description">Description:</label>
+                <textarea
+                  class="form-control"
+                  rows="5"
+                  id="description"
+                  placeholder="How to make"
+                ></textarea>
+              </div>
+              <div class="custom-file mb-3">
+      <input type="file" class="custom-file-input" id="customFile" name="filename"/>
+      <label class="custom-file-label" for="image">Choose Image</label>
+    </div>
+
+              <Link to={"/"}>
+                <button type="submit" className="btn btn-primary">
+                  <span>Add Recipe</span>
+                </button>
+              </Link>
+            </form>
           </div>
-
-          <div class="form-group">
-            <label for="category">Category</label>
-            <select id="category" class="form-control">
-              <option selected>Choose...</option>
-              <option>Sinhalese</option>
-              <option>Tamil</option>
-              <option>Muslim</option>
-            </select>
-          </div>
-
-          <div class="form-group">
-            <label for="ingredients">Ingredients</label>
-            <input
-              type="text"
-              class="form-control"
-              id="ingredients"
-              placeholder="Enter ingredients"
-            />
-          </div>
-
-          <div class="form-group">
-            <label for="description">Description:</label>
-            <textarea
-              class="form-control"
-              rows="5"
-              id="description"
-              placeholder="How to make"
-            ></textarea>
-          </div>
-
-          <Link to={"/"}>
-            <button type="submit" className="btn flex">
-              <span>Add Recipe</span>
-            </button>
-          </Link>
-        </form>
+        </div>
       </div>
     </div>
-    </Box>
-    </Grid>
   );
 };
 
