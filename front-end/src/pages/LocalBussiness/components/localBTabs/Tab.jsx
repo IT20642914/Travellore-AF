6d623928f40327bc644fb172bb356b293ee031b7
Sachini Sathsarani Wijesinghe <it20641092@my.sliit.lk>
2023-05-07 20:41:13 +0530

    update Tab and gitignore file

--- front-end/src/pages/LocalBussiness/components/localBTabs/Tab.jsx
+++ front-end/src/pages/LocalBussiness/components/localBTabs/Tab.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
+import Kulubadu from "../kulubadu/Kulubadu"
+import Handcraft from "../postHandcrafts/Handcraft"
+import Box from '@mui/material/Box';
 
 export default function () {
+
+    const [page, setPage] = useState("home");
+
+
+    const handleClick = (pageName) => {
+        setPage(pageName);
+      };
+
+
+
   return (
     <div>
         <ul class="nav nav-pills nav-fill">
   <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
+    <a class="nav-link active" aria-current="page" onClick={()=>{ handleClick("k")}} href="#">Active</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="#">Much longer nav link</a>
+    <a class="nav-link" onClick={()=>{ handleClick("h")}} href="#">Much longer nav link</a>
   </li>
   <li class="nav-item">
     <a class="nav-link" href="#">Link</a>
@@ -17,6 +30,13 @@ export default function () {
     <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
   </li>
 </ul>
+<Box>
+    aaaaaaaaaaaaaaaaaaaaaa
+               {page === "k" && <Kulubadu/>}
+               {page === "h" && <Handcraft/>}
+              
+      </Box>
+
     </div>
   )
 }
