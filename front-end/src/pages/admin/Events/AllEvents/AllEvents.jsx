a71c2932bc09ddcc74ff89b927026efb7c04e1e7
avishka05 <88711826+avishka05@users.noreply.github.com>
2023-05-13 18:17:49 +0530

    fix:

--- front-end/src/pages/admin/Events/AllEvents/AllEvents.jsx
+++ front-end/src/pages/admin/Events/AllEvents/AllEvents.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import './events.css';
 import { useNavigate } from 'react-router-dom';
 // import getCurrentUser from '../../../../utils/getCurrentUser';
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+//import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 // import newRequest from '../../../../utils/newRequest';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import EditIcon from '@mui/icons-material/Edit';
